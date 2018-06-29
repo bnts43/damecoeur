bd3ff58a11e0067503c292233bde3900dd0f9015
Benoît Neuts <benoit.neuts.pro@gmail.com>
2018-06-29 13:33:51 +0200

    Initial Commit

---
+++ niouApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  niouApp
+//
+//  Created by Benoît Neuts on 29/06/2018.
+//  Copyright © 2018 Benoît Neuts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
